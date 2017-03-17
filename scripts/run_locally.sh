5f89e6e4fb5ad517ac67f16be740b2208fb29458
Josh Long <josh@joshlong.com>
2017-03-17 11:39:09 -0500

    fixed the script to run locally

--- scripts/run_locally.sh
+++ scripts/run_locally.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-json=$1
-./drop_tables.sh
-export GOPATH=`cd ../.. && pwd`
-export ROOT_SERVICE_ACCOUNT_JSON=`cat $1`
+json=${1:-${HOME}/Desktop/gcp.json}
+
+script_dir=`dirname $0`
+pushd $script_dir/../../.. > /dev/null
+export GOPATH=`pwd`
+popd > /dev/null
+
+$script_dir/drop_tables.sh
+
+export ROOT_SERVICE_ACCOUNT_JSON="$(< $json)"
+
+m=$GOPATH/src/gcp-service-broker/manifest.yml
+
+[[ -f $m ]] || echo "found it"
+export SERVICES=$(ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))' ${m}| jq -r '.applications[0].env.SERVICES')
+
+echo $SERVICES
+
+export PRECONFIGURED_PLANS=$(ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))' ${m}| jq -r '.applications[0].env.PRECONFIGURED_PLANS')
+
+echo $PRECONFIGURED_PLANS
 export SECURITY_USER_NAME=admin
 export SECURITY_USER_PASSWORD=admin
 export DB_HOST=localhost
@@ -10,4 +27,4 @@ export DB_USERNAME=gcp-service-broker
 export DB_PASSWORD=qwerty
 echo $GOPATH
 echo $ROOT_SERVICE_ACCOUNT_JSON
-go run server.go
+go run $GOPATH/src/gcp-service-broker/server.go
