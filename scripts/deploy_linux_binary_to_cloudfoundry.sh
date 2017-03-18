6af540552946aa402b2c0cd3f361b4833ae86841
Ray Tsang <rayt@google.com>
2017-03-18 14:37:24 -0400

    Fixed the script to be more generic

--- scripts/deploy_linux_binary_to_cloudfoundry.sh
+++ scripts/deploy_linux_binary_to_cloudfoundry.sh
@@ -5,13 +5,11 @@ upload_dir=$script_dir/upload
 mkdir -p $upload_dir
 
 function stage(){
-
   pushd $script_dir/../../.. > /dev/null
   export GOPATH=`pwd`
   popd > /dev/null
 
   binary=$GOPATH/bin/gcp-service-broker
-  # rm -rf $binary
 
   cd $script_dir
   [[ -f $binary  ]] || ./build_for_linux.sh
@@ -19,18 +17,15 @@ function stage(){
   cp $binary $upload_dir
   echo "web: ./gcp-service-broker" > $upload_dir/Procfile
   cp $script_dir/../manifest-linux-binary.yml $upload_dir/manifest.yml
-  ls -la
   echo $upload_dir
 }
-
 
 function app_domain(){
     D=`cf apps | grep $1 | tr -s ' ' | cut -d' ' -f 6 | cut -d, -f1`
     echo $D
 }
 
-
-function  deploy() {
+function deploy() {
   json_file=$1
   cd $upload_dir
   echo $json_file
@@ -42,7 +37,6 @@ function  deploy() {
   db_service_plan=512mb
 
   # reset
-
   cf d -f $sb_an
   cf ds -f $mysql_db
 
@@ -56,7 +50,6 @@ function  deploy() {
   cf env $sb_an > json
 
   json=$(cat json |sed -e '1,/System-Provided:/d' | sed -e '/^}/q')
-
 
   dbHostname=$( echo $json | jq -r .VCAP_SERVICES.\"${db_service_name}\"[0].credentials.hostname )
   dbName=$( echo $json | jq -r .VCAP_SERVICES.\"${db_service_name}\"[0].credentials.name )
@@ -73,14 +66,10 @@ function  deploy() {
   cf set-env $sb_an DB_USERNAME $dbUser
   cf set-env $sb_an SECURITY_USER_NAME admin
   cf set-env $sb_an SECURITY_USER_PASSWORD admin
-
-
-  echo "about to set JSON"
   cf set-env $sb_an ROOT_SERVICE_ACCOUNT_JSON "$(< $json_file)"
 
   route=`app_domain $sb_an`
   echo $route
-
 
   cf restart $sb_an
 
@@ -89,5 +78,7 @@ function  deploy() {
 
 }
 
+[[ -f $1 ]]  || ( echo "You must pass in the path to Service Account JSON key. '$1' does not exist." && exit )
+
 stage
-deploy $HOME/Desktop/gcp.json
+deploy `pwd`/$1
