30ae33aa698f74d831a32fa5a16f34448bfef9a2
Colleen Briant <cbriant@google.com>
2017-05-22 09:52:17 -0700

    update build tile to exclude examples dir

--- ci/tasks/build-tile.sh
+++ ci/tasks/build-tile.sh
@@ -6,7 +6,7 @@ export service_broker_dir=src/gcp-service-broker
 export current_version="$(cat version/version)"
 
 pushd "$service_broker_dir"
-    zip /tmp/gcp-service-broker.zip -r . -x *.git* product/\* release/\*
+    zip /tmp/gcp-service-broker.zip -r . -x *.git* product/\* release/\* examples/\*
 
     tile build "$current_version"
 popd
