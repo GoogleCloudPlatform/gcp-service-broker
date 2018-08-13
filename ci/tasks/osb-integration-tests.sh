da5912d1b19f6d5d333e67f771be2b8d0bd883c0
Joseph Lewis III <jlewisiii@google.com>
2018-08-13 16:14:53 -0700

    added support for the new style e2e tests

---
+++ ci/tasks/osb-integration-tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Setup Environment
+GODIR=$GOPATH/src/github.com/GoogleCloudPlatform/gcp-service-broker
+mkdir -p $GOPATH/src/github.com/GoogleCloudPlatform
+ln -s $PWD/src/gcp-service-broker $GODIR
+cd $GODIR
+
+# Run Tests
+go build
+
+export SECURITY_USER_NAME=user
+export SECURITY_USER_PASSWORD=password
+
+echo "Starting server"
+./gcp-service-broker serve &
+
+sleep 5
+./gcp-service-broker client run-examples
