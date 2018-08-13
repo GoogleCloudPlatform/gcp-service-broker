913364a5a9b7e943aed5773db138a53ee6895c18
Joseph Lewis III <jlewisiii@google.com>
2018-08-13 15:14:17 -0700

    fix path in unit-tests

--- ci/tasks/unit-tests.sh
+++ ci/tasks/unit-tests.sh
@@ -2,15 +2,19 @@
 
 set -e
 
+ls -lah
+
 PROGNAME=gcp-service-broker
 GODIR=github.com/GoogleCloudPlatform/gcp-service-broker
 
 mkdir -p $GOPATH/src/github.com/GoogleCloudPlatform
-ln -s $PWD $GOPATH/src/$GODIR
+ln -s $PWD/src/gcp-service-broker $GOPATH/src/$GODIR
 
 
 go get github.com/onsi/ginkgo/ginkgo
 
 cd "${GOPATH}/src/${GODIR}"
+
+ls -lah
 
 ginkgo -r -race -skipPackage=integration,db_service .
