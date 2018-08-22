c0cfcfb8ae229930ecd40a7e180430412cb4c348
Joseph Lewis III <joseph@josephlewis.net>
2018-08-22 12:38:01 -0700

    Fixed integration test issues exposed by merges. (#264)
    Went back to mocking account manager and make it so the db integration test doesn't use the default database table which makes the other broker tests fail to start on the next test.
    
    I also added generate jobs for the fakes so they will stay up to date with changes.

--- brokerapi/brokers/models/modelsfakes/fake_service_account_manager.go
+++ brokerapi/brokers/models/modelsfakes/fake_service_account_manager.go
@@ -8,7 +8,7 @@ import (
 	"github.com/pivotal-cf/brokerapi"
 )
 
-type FakeAccountManager struct {
+type FakeServiceAccountManager struct {
 	CreateCredentialsStub        func(instanceID string, bindingID string, details brokerapi.BindDetails, instance models.ServiceInstanceDetails) (models.ServiceBindingCredentials, error)
 	createCredentialsMutex       sync.RWMutex
 	createCredentialsArgsForCall []struct {
@@ -50,11 +50,25 @@ type FakeAccountManager struct {
 		result1 map[string]string
 		result2 error
 	}
+	CreateAccountWithRolesStub        func(bindingID string, roles []string) (models.ServiceBindingCredentials, error)
+	createAccountWithRolesMutex       sync.RWMutex
+	createAccountWithRolesArgsForCall []struct {
+		bindingID string
+		roles     []string
+	}
+	createAccountWithRolesReturns struct {
+		result1 models.ServiceBindingCredentials
+		result2 error
+	}
+	createAccountWithRolesReturnsOnCall map[int]struct {
+		result1 models.ServiceBindingCredentials
+		result2 error
+	}
 	invocations      map[string][][]interface{}
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeAccountManager) CreateCredentials(instanceID string, bindingID string, details brokerapi.BindDetails, instance models.ServiceInstanceDetails) (models.ServiceBindingCredentials, error) {
+func (fake *FakeServiceAccountManager) CreateCredentials(instanceID string, bindingID string, details brokerapi.BindDetails, instance models.ServiceInstanceDetails) (models.ServiceBindingCredentials, error) {
 	fake.createCredentialsMutex.Lock()
 	ret, specificReturn := fake.createCredentialsReturnsOnCall[len(fake.createCredentialsArgsForCall)]
 	fake.createCredentialsArgsForCall = append(fake.createCredentialsArgsForCall, struct {
@@ -74,19 +88,19 @@ func (fake *FakeAccountManager) CreateCredentials(instanceID string, bindingID s
 	return fake.createCredentialsReturns.result1, fake.createCredentialsReturns.result2
 }
 
-func (fake *FakeAccountManager) CreateCredentialsCallCount() int {
+func (fake *FakeServiceAccountManager) CreateCredentialsCallCount() int {
 	fake.createCredentialsMutex.RLock()
 	defer fake.createCredentialsMutex.RUnlock()
 	return len(fake.createCredentialsArgsForCall)
 }
 
-func (fake *FakeAccountManager) CreateCredentialsArgsForCall(i int) (string, string, brokerapi.BindDetails, models.ServiceInstanceDetails) {
+func (fake *FakeServiceAccountManager) CreateCredentialsArgsForCall(i int) (string, string, brokerapi.BindDetails, models.ServiceInstanceDetails) {
 	fake.createCredentialsMutex.RLock()
 	defer fake.createCredentialsMutex.RUnlock()
 	return fake.createCredentialsArgsForCall[i].instanceID, fake.createCredentialsArgsForCall[i].bindingID, fake.createCredentialsArgsForCall[i].details, fake.createCredentialsArgsForCall[i].instance
 }
 
-func (fake *FakeAccountManager) CreateCredentialsReturns(result1 models.ServiceBindingCredentials, result2 error) {
+func (fake *FakeServiceAccountManager) CreateCredentialsReturns(result1 models.ServiceBindingCredentials, result2 error) {
 	fake.CreateCredentialsStub = nil
 	fake.createCredentialsReturns = struct {
 		result1 models.ServiceBindingCredentials
@@ -94,7 +108,7 @@ func (fake *FakeAccountManager) CreateCredentialsReturns(result1 models.ServiceB
 	}{result1, result2}
 }
 
-func (fake *FakeAccountManager) CreateCredentialsReturnsOnCall(i int, result1 models.ServiceBindingCredentials, result2 error) {
+func (fake *FakeServiceAccountManager) CreateCredentialsReturnsOnCall(i int, result1 models.ServiceBindingCredentials, result2 error) {
 	fake.CreateCredentialsStub = nil
 	if fake.createCredentialsReturnsOnCall == nil {
 		fake.createCredentialsReturnsOnCall = make(map[int]struct {
@@ -108,7 +122,7 @@ func (fake *FakeAccountManager) CreateCredentialsReturnsOnCall(i int, result1 mo
 	}{result1, result2}
 }
 
-func (fake *FakeAccountManager) DeleteCredentials(creds models.ServiceBindingCredentials) error {
+func (fake *FakeServiceAccountManager) DeleteCredentials(creds models.ServiceBindingCredentials) error {
 	fake.deleteCredentialsMutex.Lock()
 	ret, specificReturn := fake.deleteCredentialsReturnsOnCall[len(fake.deleteCredentialsArgsForCall)]
 	fake.deleteCredentialsArgsForCall = append(fake.deleteCredentialsArgsForCall, struct {
@@ -125,26 +139,26 @@ func (fake *FakeAccountManager) DeleteCredentials(creds models.ServiceBindingCre
 	return fake.deleteCredentialsReturns.result1
 }
 
-func (fake *FakeAccountManager) DeleteCredentialsCallCount() int {
+func (fake *FakeServiceAccountManager) DeleteCredentialsCallCount() int {
 	fake.deleteCredentialsMutex.RLock()
 	defer fake.deleteCredentialsMutex.RUnlock()
 	return len(fake.deleteCredentialsArgsForCall)
 }
 
-func (fake *FakeAccountManager) DeleteCredentialsArgsForCall(i int) models.ServiceBindingCredentials {
+func (fake *FakeServiceAccountManager) DeleteCredentialsArgsForCall(i int) models.ServiceBindingCredentials {
 	fake.deleteCredentialsMutex.RLock()
 	defer fake.deleteCredentialsMutex.RUnlock()
 	return fake.deleteCredentialsArgsForCall[i].creds
 }
 
-func (fake *FakeAccountManager) DeleteCredentialsReturns(result1 error) {
+func (fake *FakeServiceAccountManager) DeleteCredentialsReturns(result1 error) {
 	fake.DeleteCredentialsStub = nil
 	fake.deleteCredentialsReturns = struct {
 		result1 error
 	}{result1}
 }
 
-func (fake *FakeAccountManager) DeleteCredentialsReturnsOnCall(i int, result1 error) {
+func (fake *FakeServiceAccountManager) DeleteCredentialsReturnsOnCall(i int, result1 error) {
 	fake.DeleteCredentialsStub = nil
 	if fake.deleteCredentialsReturnsOnCall == nil {
 		fake.deleteCredentialsReturnsOnCall = make(map[int]struct {
@@ -156,7 +170,7 @@ func (fake *FakeAccountManager) DeleteCredentialsReturnsOnCall(i int, result1 er
 	}{result1}
 }
 
-func (fake *FakeAccountManager) BuildInstanceCredentials(bindRecord models.ServiceBindingCredentials, instanceRecord models.ServiceInstanceDetails) (map[string]string, error) {
+func (fake *FakeServiceAccountManager) BuildInstanceCredentials(bindRecord models.ServiceBindingCredentials, instanceRecord models.ServiceInstanceDetails) (map[string]string, error) {
 	fake.buildInstanceCredentialsMutex.Lock()
 	ret, specificReturn := fake.buildInstanceCredentialsReturnsOnCall[len(fake.buildInstanceCredentialsArgsForCall)]
 	fake.buildInstanceCredentialsArgsForCall = append(fake.buildInstanceCredentialsArgsForCall, struct {
@@ -174,19 +188,19 @@ func (fake *FakeAccountManager) BuildInstanceCredentials(bindRecord models.Servi
 	return fake.buildInstanceCredentialsReturns.result1, fake.buildInstanceCredentialsReturns.result2
 }
 
-func (fake *FakeAccountManager) BuildInstanceCredentialsCallCount() int {
+func (fake *FakeServiceAccountManager) BuildInstanceCredentialsCallCount() int {
 	fake.buildInstanceCredentialsMutex.RLock()
 	defer fake.buildInstanceCredentialsMutex.RUnlock()
 	return len(fake.buildInstanceCredentialsArgsForCall)
 }
 
-func (fake *FakeAccountManager) BuildInstanceCredentialsArgsForCall(i int) (models.ServiceBindingCredentials, models.ServiceInstanceDetails) {
+func (fake *FakeServiceAccountManager) BuildInstanceCredentialsArgsForCall(i int) (models.ServiceBindingCredentials, models.ServiceInstanceDetails) {
 	fake.buildInstanceCredentialsMutex.RLock()
 	defer fake.buildInstanceCredentialsMutex.RUnlock()
 	return fake.buildInstanceCredentialsArgsForCall[i].bindRecord, fake.buildInstanceCredentialsArgsForCall[i].instanceRecord
 }
 
-func (fake *FakeAccountManager) BuildInstanceCredentialsReturns(result1 map[string]string, result2 error) {
+func (fake *FakeServiceAccountManager) BuildInstanceCredentialsReturns(result1 map[string]string, result2 error) {
 	fake.BuildInstanceCredentialsStub = nil
 	fake.buildInstanceCredentialsReturns = struct {
 		result1 map[string]string
@@ -194,7 +208,7 @@ func (fake *FakeAccountManager) BuildInstanceCredentialsReturns(result1 map[stri
 	}{result1, result2}
 }
 
-func (fake *FakeAccountManager) BuildInstanceCredentialsReturnsOnCall(i int, result1 map[string]string, result2 error) {
+func (fake *FakeServiceAccountManager) BuildInstanceCredentialsReturnsOnCall(i int, result1 map[string]string, result2 error) {
 	fake.BuildInstanceCredentialsStub = nil
 	if fake.buildInstanceCredentialsReturnsOnCall == nil {
 		fake.buildInstanceCredentialsReturnsOnCall = make(map[int]struct {
@@ -208,7 +222,64 @@ func (fake *FakeAccountManager) BuildInstanceCredentialsReturnsOnCall(i int, res
 	}{result1, result2}
 }
 
-func (fake *FakeAccountManager) Invocations() map[string][][]interface{} {
+func (fake *FakeServiceAccountManager) CreateAccountWithRoles(bindingID string, roles []string) (models.ServiceBindingCredentials, error) {
+	var rolesCopy []string
+	if roles != nil {
+		rolesCopy = make([]string, len(roles))
+		copy(rolesCopy, roles)
+	}
+	fake.createAccountWithRolesMutex.Lock()
+	ret, specificReturn := fake.createAccountWithRolesReturnsOnCall[len(fake.createAccountWithRolesArgsForCall)]
+	fake.createAccountWithRolesArgsForCall = append(fake.createAccountWithRolesArgsForCall, struct {
+		bindingID string
+		roles     []string
+	}{bindingID, rolesCopy})
+	fake.recordInvocation("CreateAccountWithRoles", []interface{}{bindingID, rolesCopy})
+	fake.createAccountWithRolesMutex.Unlock()
+	if fake.CreateAccountWithRolesStub != nil {
+		return fake.CreateAccountWithRolesStub(bindingID, roles)
+	}
+	if specificReturn {
+		return ret.result1, ret.result2
+	}
+	return fake.createAccountWithRolesReturns.result1, fake.createAccountWithRolesReturns.result2
+}
+
+func (fake *FakeServiceAccountManager) CreateAccountWithRolesCallCount() int {
+	fake.createAccountWithRolesMutex.RLock()
+	defer fake.createAccountWithRolesMutex.RUnlock()
+	return len(fake.createAccountWithRolesArgsForCall)
+}
+
+func (fake *FakeServiceAccountManager) CreateAccountWithRolesArgsForCall(i int) (string, []string) {
+	fake.createAccountWithRolesMutex.RLock()
+	defer fake.createAccountWithRolesMutex.RUnlock()
+	return fake.createAccountWithRolesArgsForCall[i].bindingID, fake.createAccountWithRolesArgsForCall[i].roles
+}
+
+func (fake *FakeServiceAccountManager) CreateAccountWithRolesReturns(result1 models.ServiceBindingCredentials, result2 error) {
+	fake.CreateAccountWithRolesStub = nil
+	fake.createAccountWithRolesReturns = struct {
+		result1 models.ServiceBindingCredentials
+		result2 error
+	}{result1, result2}
+}
+
+func (fake *FakeServiceAccountManager) CreateAccountWithRolesReturnsOnCall(i int, result1 models.ServiceBindingCredentials, result2 error) {
+	fake.CreateAccountWithRolesStub = nil
+	if fake.createAccountWithRolesReturnsOnCall == nil {
+		fake.createAccountWithRolesReturnsOnCall = make(map[int]struct {
+			result1 models.ServiceBindingCredentials
+			result2 error
+		})
+	}
+	fake.createAccountWithRolesReturnsOnCall[i] = struct {
+		result1 models.ServiceBindingCredentials
+		result2 error
+	}{result1, result2}
+}
+
+func (fake *FakeServiceAccountManager) Invocations() map[string][][]interface{} {
 	fake.invocationsMutex.RLock()
 	defer fake.invocationsMutex.RUnlock()
 	fake.createCredentialsMutex.RLock()
@@ -217,6 +288,8 @@ func (fake *FakeAccountManager) Invocations() map[string][][]interface{} {
 	defer fake.deleteCredentialsMutex.RUnlock()
 	fake.buildInstanceCredentialsMutex.RLock()
 	defer fake.buildInstanceCredentialsMutex.RUnlock()
+	fake.createAccountWithRolesMutex.RLock()
+	defer fake.createAccountWithRolesMutex.RUnlock()
 	copiedInvocations := map[string][][]interface{}{}
 	for key, value := range fake.invocations {
 		copiedInvocations[key] = value
@@ -224,7 +297,7 @@ func (fake *FakeAccountManager) Invocations() map[string][][]interface{} {
 	return copiedInvocations
 }
 
-func (fake *FakeAccountManager) recordInvocation(key string, args []interface{}) {
+func (fake *FakeServiceAccountManager) recordInvocation(key string, args []interface{}) {
 	fake.invocationsMutex.Lock()
 	defer fake.invocationsMutex.Unlock()
 	if fake.invocations == nil {
@@ -236,4 +309,4 @@ func (fake *FakeAccountManager) recordInvocation(key string, args []interface{})
 	fake.invocations[key] = append(fake.invocations[key], args)
 }
 
-var _ models.AccountManager = new(FakeAccountManager)
+var _ models.ServiceAccountManager = new(FakeServiceAccountManager)
