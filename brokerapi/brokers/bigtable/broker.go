c378c273202d401fb25489b5d64c9c96349fc4f6
Joseph Lewis III <jlewisiii@google.com>
2018-10-09 16:25:15 -0700

    migrated bigtable to using templated vars

--- brokerapi/brokers/bigtable/broker.go
+++ brokerapi/brokers/bigtable/broker.go
@@ -17,12 +17,10 @@ package bigtable
 import (
 	"encoding/json"
 	"fmt"
-	"strconv"
 
 	googlebigtable "cloud.google.com/go/bigtable"
 	"github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/broker_base"
 	"github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/models"
-	"github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/name_generator"
 	"github.com/pivotal-cf/brokerapi"
 	"golang.org/x/net/context"
 	"google.golang.org/api/option"
@@ -48,18 +46,24 @@ var storageTypes = map[string]googlebigtable.StorageType{
 
 // Provision creates a new Bigtable instance from the settings in the user-provided details and service plan.
 func (b *BigTableBroker) Provision(ctx context.Context, instanceId string, details brokerapi.ProvisionDetails, plan models.ServicePlan) (models.ServiceInstanceDetails, error) {
-	var err error
-	var params map[string]string
-
-	if len(details.RawParameters) == 0 {
-		params = map[string]string{}
-	} else if err = json.Unmarshal(details.RawParameters, &params); err != nil {
-		return models.ServiceInstanceDetails{}, fmt.Errorf("Error unmarshalling parameters: %s", err)
+	provisionContext, err := serviceDefinition().ProvisionVariables(instanceId, details, plan)
+	if err != nil {
+		return models.ServiceInstanceDetails{}, err
 	}
 
-	// Ensure there is a name for this instance
-	if _, ok := params["name"]; !ok {
-		params["name"] = name_generator.Basic.InstanceNameWithSeparator("-")
+	instanceName := provisionContext.GetString("name")
+
+	ic := googlebigtable.InstanceConf{
+		InstanceId:  instanceName,
+		ClusterId:   provisionContext.GetString("cluster_id"),
+		NumNodes:    int32(provisionContext.GetInt("num_nodes")),
+		StorageType: storageTypes[provisionContext.GetString("storage_type")],
+		Zone:        provisionContext.GetString("zone"),
+		DisplayName: provisionContext.GetString("display_name"),
+	}
+
+	if err := provisionContext.Error(); err != nil {
+		return models.ServiceInstanceDetails{}, err
 	}
 
 	service, err := b.createClient(ctx)
@@ -67,47 +71,12 @@ func (b *BigTableBroker) Provision(ctx context.Context, instanceId string, detai
 		return models.ServiceInstanceDetails{}, err
 	}
 
-	var clusterId string
-	if len(params["name"]) > 20 {
-		clusterId = params["name"][:20] + "-cluster"
-	} else {
-		clusterId = params["name"] + "-cluster"
-	}
-	if userClusterId, clusterIdOk := params["cluster_id"]; clusterIdOk {
-		clusterId = userClusterId
-	}
-
-	numNodes, err := strconv.Atoi(plan.ServiceProperties["num_nodes"])
-	if err != nil {
-		return models.ServiceInstanceDetails{}, fmt.Errorf("Error converting num_nodes to int: %s", err)
-	}
-
-	zone := "us-east1-b"
-	if userZone, userZoneOk := params["zone"]; userZoneOk {
-		zone = userZone
-	}
-
-	displayName := params["name"]
-	if userDisplayName, userDisplayNameOk := params["display_name"]; userDisplayNameOk {
-		displayName = userDisplayName
-	}
-
-	ic := googlebigtable.InstanceConf{
-		InstanceId:  params["name"],
-		ClusterId:   clusterId,
-		NumNodes:    int32(numNodes),
-		StorageType: storageTypes[plan.ServiceProperties["storage_type"]],
-		Zone:        zone,
-		DisplayName: displayName,
-	}
-
-	err = service.CreateInstance(ctx, &ic)
-	if err != nil {
-		return models.ServiceInstanceDetails{}, fmt.Errorf("Error creating new instance: %s", err)
+	if err := service.CreateInstance(ctx, &ic); err != nil {
+		return models.ServiceInstanceDetails{}, fmt.Errorf("Error creating new Bigtable instance: %s", err)
 	}
 
 	ii := InstanceInformation{
-		InstanceId: params["name"],
+		InstanceId: instanceName,
 	}
 
 	otherDetails, err := json.Marshal(ii)
@@ -115,14 +84,12 @@ func (b *BigTableBroker) Provision(ctx context.Context, instanceId string, detai
 		return models.ServiceInstanceDetails{}, fmt.Errorf("Error marshalling other details: %s", err)
 	}
 
-	i := models.ServiceInstanceDetails{
-		Name:         params["name"],
+	return models.ServiceInstanceDetails{
+		Name:         instanceName,
 		Url:          "",
 		Location:     "",
 		OtherDetails: string(otherDetails),
-	}
-
-	return i, nil
+	}, nil
 }
 
 // Deprovision deletes the BigTable associated with the given instance.
@@ -133,7 +100,7 @@ func (b *BigTableBroker) Deprovision(ctx context.Context, instance models.Servic
 	}
 
 	if err := service.DeleteInstance(ctx, instance.Name); err != nil {
-		return fmt.Errorf("Error deleting dataset: %s", err)
+		return fmt.Errorf("Error deleting Bigtable instance: %s", err)
 	}
 
 	return nil
