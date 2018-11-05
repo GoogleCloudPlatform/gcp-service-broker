bbc151335ad02917cce49910b6c5cbfbe2efa838
Joseph Lewis III <joseph@josephlewis.net>
2018-11-05 10:47:19 -0800

    added Firestore support (#343)
    * added firestore support

--- brokerapi/brokers/gcp_service_broker.go
+++ brokerapi/brokers/gcp_service_broker.go
@@ -36,6 +36,7 @@ import (
 	_ "github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/cloudsql"
 	_ "github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/datastore"
 	_ "github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/dialogflow"
+	_ "github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/firestore"
 	_ "github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/pubsub"
 	_ "github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/spanner"
 	_ "github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/stackdriver"
