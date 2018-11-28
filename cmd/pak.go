22578fcd99cc9ccc9ea23b9c38b043f15fa6522c
Joseph Lewis III <jlewisiii@google.com>
2018-11-28 08:40:46 -0800

    better pack commands

--- cmd/pak.go
+++ cmd/pak.go
@@ -23,7 +23,45 @@ func init() {
 	pakCmd := &cobra.Command{
 		Use:   "pak",
 		Short: "interact with user-defined service definition bundles",
-		Long:  `interact with user-defined service definition bundles`,
+		Long: `Lets you create, validate, and view service definition bundles.
+
+A service definition bundle is a zip file containing all the elements needed
+to define and run a custom service.
+
+Bundles include source code (for legal compliance), service definitions, and
+Terraform/provider binaries for multiple platforms. They give you a contained
+way to deploy new services to existing brokers or augment the broker to fit
+your needs.
+
+To start building a pack, create a new directory and within it run init:
+
+	mkdir my-pak && cd my-pak
+	gcp-service-broker pak init
+
+You'll get a new pack with a manifest and example service definition.
+Define the architectures and Terraform plugins you need in your manifest along
+with any metadata you want, and include the names of all service definition
+files.
+
+When you're done, you can build the bundle which will download the sources,
+Terraform resources, and pack them together.
+
+	cd .. && gcp-service-broker pak build my-pak
+
+This will produce a pack:
+
+	my-pak.zip
+
+You can validate the pack:
+
+	gcp-service-broker pak validate my-pak.zip
+
+You can also list information about the pack which includes metadata,
+dependencies, services it provides, and the contents.
+
+	gcp-service-broker pak info my-pak.zip
+
+`,
 		Run: func(cmd *cobra.Command, args []string) {
 			cmd.Help()
 		},
@@ -40,24 +78,32 @@ func init() {
 	})
 
 	pakCmd.AddCommand(&cobra.Command{
-		Use:   "build",
+		Use:   "build [path/to/pack/directory]",
 		Short: "bundle up the service definition files and Terraform resources",
+		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return brokerpak.Pack("")
+			directory := ""
+			if len(args) == 1 {
+				directory = args[0]
+			}
+
+			return brokerpak.Pack(directory)
 		},
 	})
 
 	pakCmd.AddCommand(&cobra.Command{
-		Use:   "info pack",
+		Use:   "info [pack.zip]",
 		Short: "get info about a service definition pack",
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return brokerpak.Info(args[0])
 		},
 	})
 
 	pakCmd.AddCommand(&cobra.Command{
-		Use:   "validate pack",
+		Use:   "validate [pack.zip]",
 		Short: "validate a service definition pack",
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return brokerpak.Validate(args[0])
 		},
