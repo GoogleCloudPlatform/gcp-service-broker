a63dd5d7da1753f41bd3c5a7b1a28cd336ada07c
Joseph Lewis III <jlewisiii@google.com>
2018-11-29 10:43:05 -0800

    moved documentation to use a specific registry

--- cmd/generate.go
+++ cmd/generate.go
@@ -17,6 +17,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/GoogleCloudPlatform/gcp-service-broker/pkg/broker"
 	"github.com/GoogleCloudPlatform/gcp-service-broker/pkg/generator"
 	"github.com/spf13/cobra"
 )
@@ -42,7 +43,7 @@ func init() {
 
 	`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println(generator.CatalogDocumentation())
+			fmt.Println(generator.CatalogDocumentation(broker.DefaultRegistry))
 		},
 	})
 
