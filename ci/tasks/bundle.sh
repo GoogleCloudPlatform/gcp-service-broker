8088a55545c559bd812ccb09e995e6c3c82453fc
Joseph Lewis III <joseph@josephlewis.net>
2018-12-10 13:33:09 -0800

    CI Cleanup (#375)
    This is the new CI pipeline that can be found in Sunrisecafe as "gcp-service-broker-cleanup".
    
    The new pipeline has three major steps:
    
    1. Build & Unit Test
    2. Integration Test
    3. Release
    
    We use the cloud-build golang docker image to condense the work we were doing to build all over the place.
    
    Releases now include almost everything we will want to stick in our GitHub release. Release notes, SHA1 hash, tiles, CF app, and OSDF. The only thing missing are platform-specific binaries.
    
    There's one code change in this, which is to change a CLOB -> TEXT type for MySQL.

---
+++ ci/tasks/bundle.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+echo "Installing zip"
+apk update
+apk add zip
+
+export CURRENT_VERSION="$(cat metadata/version)"
+export REVISION="$(cat metadata/revision)"
+
+# Bundle up the output
+mkdir staging
+
+echo "Staging files from the source"
+cp gcp-service-broker/CHANGELOG.md staging/
+cp gcp-service-broker/OSDF*.txt staging/
+
+echo "Staging files from metadata"
+cp metadata/version staging/
+cp -r metadata/docs staging/
+
+echo "Staging server binaries"
+mkdir -p staging/servers
+cp tiles/* staging/servers
+
+echo "Staging client binaries"
+# TODO(josephlewis42) pack up cross-compiled binaries for windows/darwin/linux
+
+echo "Creating release"
+zip bundle/gcp-service-broker-$CURRENT_VERSION-$REVISION.zip -r staging/*
---
+++ ci/tasks/metadata.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+echo "Installing dependencies"
+apk update
+apk add git
+
+echo "Generating metadata"
+mkdir -p metadata/docs
+
+git --git-dir=gcp-service-broker/.git rev-parse HEAD > metadata/revision
+./compiled-broker/gcp-service-broker version > metadata/version
+./compiled-broker/gcp-service-broker generate tile > metadata/tile.yml
+./compiled-broker/gcp-service-broker generate use > metadata/manifest.yml
+./compiled-broker/gcp-service-broker generate customization > metadata/docs/customization.md
+./compiled-broker/gcp-service-broker generate use > metadata/docs/use.md
