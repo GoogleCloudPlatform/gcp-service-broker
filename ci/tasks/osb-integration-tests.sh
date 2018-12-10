0053dadc87d8ae2b8ff1e0edb4c4edc4c93d99f2
Joseph Lewis III <jlewisiii@google.com>
2018-12-10 09:21:47 -0800

    try again

--- ci/tasks/osb-integration-tests.sh
+++ ci/tasks/osb-integration-tests.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-# Setup Environment
-export SECURITY_USER_NAME=user
-export SECURITY_USER_PASSWORD=password
-export PORT=8080
+pushd compiled-broker
+  # Setup Environment
+  export SECURITY_USER_NAME=user
+  export SECURITY_USER_PASSWORD=password
+  export PORT=8080
 
-echo "Running brokerpak tests"
-./gcp-service-broker pak test
+  echo "Running brokerpak tests"
+  ./gcp-service-broker pak test
 
-echo "Starting server"
-./gcp-service-broker migrate
-./gcp-service-broker serve &
+  echo "Starting server"
+  ./gcp-service-broker migrate
+  ./gcp-service-broker serve &
 
-sleep 5
-./gcp-service-broker client run-examples
+  sleep 5
+  ./gcp-service-broker client run-examples
+popd
