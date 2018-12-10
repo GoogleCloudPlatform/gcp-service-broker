129fabb8466ee23235556e38d9c89c1c41fcd9ca
Joseph Lewis III <jlewisiii@google.com>
2018-12-10 09:28:18 -0800

    remove pushd for alpine

--- ci/tasks/osb-integration-tests.sh
+++ ci/tasks/osb-integration-tests.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-pushd compiled-broker
-  # Setup Environment
-  export SECURITY_USER_NAME=user
-  export SECURITY_USER_PASSWORD=password
-  export PORT=8080
+cd compiled-broker
 
-  echo "Running brokerpak tests"
-  ./gcp-service-broker pak test
+# Setup Environment
+export SECURITY_USER_NAME=user
+export SECURITY_USER_PASSWORD=password
+export PORT=8080
 
-  echo "Starting server"
-  ./gcp-service-broker migrate
-  ./gcp-service-broker serve &
+echo "Running brokerpak tests"
+./gcp-service-broker pak test
 
-  sleep 5
-  ./gcp-service-broker client run-examples
-popd
+echo "Starting server"
+./gcp-service-broker migrate
+./gcp-service-broker serve &
+
+sleep 5
+./gcp-service-broker client run-examples
