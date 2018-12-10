3dd9e925d5ead4e100095026610caaa3d17e4bca
Joseph Lewis III <jlewisiii@google.com>
2018-12-10 11:23:21 -0800

    fix tilegen

--- ci/tasks/build-tile.sh
+++ ci/tasks/build-tile.sh
@@ -2,16 +2,19 @@
 
 set -e
 
+export OUTPUT_DIR=$PWD/tiles
 export SERVICE_BROKER_DIR=src/gcp-service-broker
 export CURRENT_VERSION="$(cat metadata/version)"
 
+mkdir -p tiles
+
 pushd "$SERVICE_BROKER_DIR"
     zip /tmp/gcp-service-broker.zip -r . -x *.git* product/\* release/\* examples/\*
-    cp /tmp/gcp-service-broker.zip ../tiles/gcp-service-broker-$CURRENT_VERSION-cf-app.zip
+    cp /tmp/gcp-service-broker.zip $OUTPUT_DIR/gcp-service-broker-$CURRENT_VERSION-cf-app.zip
 
     tile build "$CURRENT_VERSION"
-    mv "product/"*.pivotal ../tiles/
+    mv "product/"*.pivotal $OUTPUT_DIR
 
     tile build "$CURRENT_VERSION-rc"
-    mv "product/"*.pivotal ../tiles/
+    mv "product/"*.pivotal $OUTPUT_DIR
 popd
