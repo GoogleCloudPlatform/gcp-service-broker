e71374b680379ac19926b495cf4ceeba29aa7155
Joseph Lewis III <jlewisiii@google.com>
2018-12-10 09:15:46 -0800

    added ci to deps

--- ci/tasks/osb-integration-tests.sh
+++ ci/tasks/osb-integration-tests.sh
@@ -8,11 +8,11 @@ export SECURITY_USER_PASSWORD=password
 export PORT=8080
 
 echo "Running brokerpak tests"
-./compiled-broker/gcp-service-broker pak test
+./gcp-service-broker pak test
 
 echo "Starting server"
-./compiled-broker/gcp-service-broker migrate
-./compiled-broker/gcp-service-broker serve &
+./gcp-service-broker migrate
+./gcp-service-broker serve &
 
 sleep 5
-./compiled-broker/gcp-service-broker client run-examples
+./gcp-service-broker client run-examples
