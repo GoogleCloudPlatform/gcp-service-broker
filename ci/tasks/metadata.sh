7b671e556118270ab1721c59f0022d0c2c0979ab
Joseph Lewis III <jlewisiii@google.com>
2018-12-10 10:48:26 -0800

    move docs to docs subdir

--- ci/tasks/metadata.sh
+++ ci/tasks/metadata.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-mkdir metadata
+mkdir -p metadata/docs
 
 ./compiled-broker/gcp-service-broker version > metadata/version
 ./compiled-broker/gcp-service-broker generate tile > metadata/tile.yml
-./compiled-broker/gcp-service-broker generate customization > metadata/customization.md
-./compiled-broker/gcp-service-broker generate use > metadata/use.md
 ./compiled-broker/gcp-service-broker generate use > metadata/manifest.yml
+./compiled-broker/gcp-service-broker generate customization > metadata/docs/customization.md
+./compiled-broker/gcp-service-broker generate use > metadata/docs/use.md
