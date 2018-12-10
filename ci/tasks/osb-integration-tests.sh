28a4fc6eb9a57d1bd1723adf52e90590b443d068
Joseph Lewis III <jlewisiii@google.com>
2018-12-10 09:03:39 -0800

    re-structuring tasks to split into test/integration/release

--- ci/tasks/osb-integration-tests.sh
+++ ci/tasks/osb-integration-tests.sh
@@ -3,17 +3,12 @@
 set -e
 
 # Setup Environment
-GODIR=$GOPATH/src/github.com/GoogleCloudPlatform/gcp-service-broker
-mkdir -p $GOPATH/src/github.com/GoogleCloudPlatform
-ln -s $PWD/src/gcp-service-broker $GODIR
-cd $GODIR
-
-# Run Tests
-go build
-
 export SECURITY_USER_NAME=user
 export SECURITY_USER_PASSWORD=password
 export PORT=8080
+
+echo "Running brokerpak tests"
+./gcp-service-broker pak test
 
 echo "Starting server"
 ./gcp-service-broker migrate
