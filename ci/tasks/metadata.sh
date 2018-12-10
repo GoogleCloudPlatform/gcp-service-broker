b15c22543afa0396c785108e552515a754557c0f
Joseph Lewis III <jlewisiii@google.com>
2018-12-10 08:31:15 -0800

    added metadata generation

---
+++ ci/tasks/metadata.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir metadata
+
+./compiled-broker/gcp-service-broker version > metadata/version
+./compiled-broker/gcp-service-broker generate tile > metadata/tile.yml
+./compiled-broker/gcp-service-broker generate customization > metadata/customization.md
+./compiled-broker/gcp-service-broker generate use > metadata/use.md
+./compiled-broker/gcp-service-broker generate use > metadata/manifest.yml
