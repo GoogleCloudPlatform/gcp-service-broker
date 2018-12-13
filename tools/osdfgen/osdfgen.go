17f0a07102af5ef14c4c6b5d816bef311c75ab6c
Joseph Lewis III <jlewisiii@google.com>
2018-12-13 13:45:27 -0800

    refactored osdfgen and added tests

--- tools/osdfgen/osdfgen.go
+++ tools/osdfgen/osdfgen.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// `osdfgen` can be used to build a CSV suitable for uploading to Pivotal's 
+// `osdfgen` can be used to build a CSV suitable for uploading to Pivotal's
 // [OSDF Generator](http://osdf-generator.cfapps.io/static/index.html).
 // It determines licenses by sniffing the dependencies listed in `Gopkg.lock`.
 // Example: go run osdfgen.go -p ../../ -o test.csv
@@ -20,14 +20,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
+	"math"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 
@@ -35,11 +36,6 @@ import (
 	"gopkg.in/src-d/go-license-detector.v2/licensedb/filer"
 
 	toml "github.com/pelletier/go-toml"
-)
-
-var (
-	out  = flag.String("o", "-", "Sets the output location of the OSDF csv")
-	proj = flag.String("p", ".", "The project root")
 )
 
 type Lockfile struct {
@@ -51,10 +47,85 @@ type Project struct {
 	Revision string
 }
 
+type Dependency struct {
+	ParentDirectory string
+	Project         Project
+}
+
+func (d *Dependency) Directory() string {
+	return filepath.Join(d.ParentDirectory, "vendor", d.Project.Name)
+}
+
 func main() {
+	out := flag.String("o", "-", "Sets the output location of the OSDF csv")
+	proj := flag.String("p", ".", "The project root")
+
 	flag.Parse()
 
-	tree, err := toml.LoadFile(filepath.Join(*proj, "Gopkg.lock"))
+	buf := &bytes.Buffer{}
+	writer := csv.NewWriter(buf)
+	for _, project := range getProjects(*proj) {
+		dep := &Dependency{
+			ParentDirectory: *proj,
+			Project:         project,
+		}
+
+		licenses, err := detectLicenses(dep.Directory())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		spdxCode := mostLikelyLicense(licenses)
+		licenseText, err := getLicenseText(dep, spdxCode)
+		if err != nil {
+			log.Fatalf("Could not get license text for %q: %s", project.Name, err)
+		}
+
+		// name, hash, spdx, full text
+		writer.Write([]string{project.Name, project.Revision, spdxCode, licenseText})
+	}
+
+	writer.Flush()
+	writeOutput(*out, buf)
+}
+
+// detectLicenses returns a map of the SPDX codes for the licenses in a given
+// directory.
+func detectLicenses(directory string) (map[string]float32, error) {
+	dir, err := filer.FromDirectory(directory)
+	if err != nil {
+		return nil, fmt.Errorf("Could not find dep %q in vendor %s", dir, err)
+	}
+
+	return licensedb.Detect(dir)
+}
+
+// writeOutput writes the contents of src into the file denoted by fileName.
+// if fileName is "-" the UNIX convention is followed and the contents are
+// written to stdout.
+func writeOutput(fileName string, src io.Reader) {
+	var dest io.Writer
+	if fileName == "-" {
+		dest = os.Stdout
+	} else {
+		var err error
+		fd, err := os.Create(fileName)
+		if err != nil {
+			log.Fatalf("Error opening %q, %s", fileName, err)
+		}
+		defer fd.Close()
+		dest = fd
+	}
+
+	if _, err := io.Copy(dest, src); err != nil {
+		log.Fatalf("Error trying to write output: %v", err)
+	}
+}
+
+// getProjects reads a Gopkg.lock file and returns a list of the projects it
+// contains.
+func getProjects(projectRoot string) []Project {
+	tree, err := toml.LoadFile(filepath.Join(projectRoot, "Gopkg.lock"))
 	if err != nil {
 		log.Fatalf("Error loading Gopkg.lock, %s", err)
 	}
@@ -64,45 +135,12 @@ func main() {
 		log.Fatalf("Error unmarshaling lockfile %s", err)
 	}
 
-	var writer *csv.Writer
-	if *out == "-" {
-		writer = csv.NewWriter(os.Stdout)
-	} else {
-		fd, err := os.Create(*out)
-		if err != nil {
-			log.Fatalf("Error opening %q, %s", *out, err)
-		}
-		defer fd.Close()
-		writer = csv.NewWriter(fd)
-	}
-
-	for _, project := range deps.Projects {
-		dir, err := filer.FromDirectory(filepath.Join(*proj, "vendor", project.Name))
-		if err != nil {
-			log.Fatalf("Could not find dep %q in vendor %s", project.Name, err)
-		}
-
-		licenses, err := licensedb.Detect(dir)
-		if err != nil {
-			log.Fatalf("Could not detect licenses %s", err)
-		}
-
-		spdxCode := mostLikelyLicense(licenses)
-
-		licenseText, err := getLicenseText(project, spdxCode)
-		if err != nil {
-			log.Printf("Could not get license text for %q: %s", project.Name, err)
-		}
-
-		// name, hash, spdx, full text
-		writer.Write([]string{project.Name, project.Revision, spdxCode, licenseText})
-	}
-
-	writer.Flush()
+	return deps.Projects
 }
 
+// mostLikelyLicense finds the key in a map with the greatest value.
 func mostLikelyLicense(m map[string]float32) string {
-	var maxVal float32
+	var maxVal float32 = -math.MaxFloat32
 	maxKey := ""
 
 	for k, v := range m {
@@ -115,32 +153,35 @@ func mostLikelyLicense(m map[string]float32) string {
 	return maxKey
 }
 
-func getLicenseText(project Project, spdxCode string) (string, error) {
-	projectRoot := filepath.Join(*proj, "vendor", project.Name)
-	entries, err := ioutil.ReadDir(projectRoot)
+// getLicenseText creates a copy of the licence text(s) and notice(s) for a
+// given project.
+// Returns an error if no license could be found.
+func getLicenseText(project *Dependency, spdxCode string) (string, error) {
+	dir, err := filer.FromDirectory(project.Directory())
 	if err != nil {
-		log.Fatalf("Could not find dep %q in vendor %s", project.Name, err)
+		log.Fatalf("Could not find dep %q in vendor %s", project.Project.Name, err)
+	}
+
+	entries, err := dir.ReadDir(".")
+	if err != nil {
+		log.Fatalf("Could not find dep %q in vendor %s", project.Project.Name, err)
 	}
 
 	licenses := ""
-
 	for _, entry := range entries {
-		if entry.IsDir() {
+		if !shouldIncludeFileInOsdf(entry, spdxCode) {
 			continue
 		}
 
-		base := strings.ToLower(path.Base(entry.Name()))
-		fullPath := path.Join(projectRoot, entry.Name())
-		if strings.Contains(base, "license") || (base == "notice" && spdxCode == "Apache-2.0") {
-			licenses += fmt.Sprintf("Contents of: %s@%s\n\n", fullPath, project.Revision)
-			text, err := ioutil.ReadFile(fullPath)
-			if err != nil {
-				return licenses, err
-			}
+		licenses += fmt.Sprintf("Contents of: %s/%s@%s\n\n", project.Project.Name, entry.Name, project.Project.Revision)
 
-			licenses += string(text)
-			licenses += "\n\n"
+		text, err := dir.ReadFile(entry.Name)
+		if err != nil {
+			return licenses, err
 		}
+
+		licenses += string(text)
+		licenses += "\n\n"
 	}
 
 	if licenses == "" {
@@ -148,4 +189,19 @@ func getLicenseText(project Project, spdxCode string) (string, error) {
 	}
 
 	return licenses, nil
+}
+
+// A file should be included in the OSDF if it's a license, or the license is
+// Apache and it's a notice.
+func shouldIncludeFileInOsdf(file filer.File, spdxCode string) bool {
+	if file.IsDir {
+		return false
+	}
+
+	lowerName := strings.ToLower(file.Name)
+	isLicense := strings.Contains(lowerName, "license")
+
+	// We're required to include NOTICE files for Apache 2 licensed products.
+	isNotice := lowerName == "notice" && spdxCode == "Apache-2.0"
+	return isLicense || isNotice
 }
