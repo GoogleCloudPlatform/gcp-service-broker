5e54f517268a494e62e36d280408ee5d57e1a5e6
Joseph Lewis III <joseph@josephlewis.net>
2019-01-02 15:28:56 -0800

    removed 3 to 4 compatibility code (#403)
    * removed 3 to 4 compatibility code

--- cmd/serve.go
+++ cmd/serve.go
@@ -22,7 +22,6 @@ import (
 	"github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers"
 	"github.com/GoogleCloudPlatform/gcp-service-broker/brokerapi/brokers/models"
 	"github.com/GoogleCloudPlatform/gcp-service-broker/db_service"
-	"github.com/GoogleCloudPlatform/gcp-service-broker/pkg/compatibility"
 	"github.com/GoogleCloudPlatform/gcp-service-broker/pkg/server"
 	"github.com/GoogleCloudPlatform/gcp-service-broker/pkg/toggles"
 	"github.com/GoogleCloudPlatform/gcp-service-broker/utils"
@@ -36,11 +35,6 @@ const (
 	apiPasswordProp = "api.password"
 	apiPortProp     = "api.port"
 )
-
-var v3CompatibilityToggle = toggles.Compatibility.Toggle("three-to-four.legacy-plans", false, `Enable compatibility with the GCP Service Broker v3.x.
-	Before version 4.0, each installation generated its own plan UUIDs, after 4.0 they have been standardized.
-	This option installs a compatibility layer which checks if a service is using the correct plan GUID.
-	If the service does not use the correct GUID, the request will fail with a message about how to upgrade.`)
 
 var cfCompatibilityToggle = toggles.Compatibility.Toggle("enable-cf-sharing", false, `Set all services to have the Sharable flag so they can be shared
 	across spaces in PCF.`)
@@ -98,12 +92,6 @@ func serve() {
 	if cfCompatibilityToggle.IsActive() {
 		logger.Info("Enabling Cloud Foundry service sharing")
 		serviceBroker = server.NewCfSharingWrapper(serviceBroker)
-	}
-
-	if v3CompatibilityToggle.IsActive() {
-		logger.Info("Enabling v3 Compatibility Mode")
-
-		serviceBroker = compatibility.NewLegacyPlanUpgrader(serviceBroker)
 	}
 
 	services, err := serviceBroker.Services(context.Background())
