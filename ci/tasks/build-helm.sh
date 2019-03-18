2f2c2f90771c07397e80a0525f29e9c46001ee3e
Joseph Lewis III <jlewisiii@google.com>
2019-03-18 14:56:10 -0700

    fixed path

--- ci/tasks/build-helm.sh
+++ ci/tasks/build-helm.sh
@@ -2,8 +2,5 @@
 
 VERSION="$(cat metadata/version)"
 
-echo "dir"
-ls
-
 echo "packaging version: $VERSION"
 helm package --app-version=$VERSION --dependency-update --version=$VERSION --destination helm-chart "gcp-service-broker/deployments/helm/gcp-service-broker"
