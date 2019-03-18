bdb95c5cb5edf27f194412ef00b7eda12c472493
Joseph Lewis III <jlewisiii@google.com>
2019-03-18 15:01:46 -0700

    another update

--- ci/tasks/build-helm.sh
+++ ci/tasks/build-helm.sh
@@ -3,4 +3,5 @@
 VERSION="$(cat metadata/version)"
 
 echo "packaging version: $VERSION"
+helm init --client-only
 helm package --app-version=$VERSION --dependency-update --version=$VERSION --destination helm-chart "gcp-service-broker/deployments/helm/gcp-service-broker"
