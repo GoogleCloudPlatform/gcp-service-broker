e528f051d5511a2381573ca737fda78c49275c6c
Sophia Hu <hsophia@google.com>
2019-07-01 17:26:54 -0700

    Add checks for single MySQL VCAP_SERVICE

--- db_service/vcap.go
+++ db_service/vcap.go
@@ -8,10 +8,6 @@ import (
 	"os"
 )
 
-type VcapServiceMap struct {
-	VcapServiceMap map[string][]VcapService
-}
-
 type VcapService struct {
 	BindingName  string            `json:"binding_name"`  // The name assigned to the service binding by the user.
 	InstanceName string            `json:"instance_name"` // The name assigned to the service instance by the user.
@@ -34,13 +30,16 @@ func useVcapServices(logger lager.Logger) {
 
 		viper.Set(dbPathProp, u.Path)
 		viper.Set(dbTypeProp, DbTypeMysql)
-		viper.Set(caCertProp, vcapService.Credentials["CaCert"])
-		viper.Set(clientCertProp, vcapService.Credentials["ClientCert"])
-		viper.Set(clientKeyProp, vcapService.Credentials["ClientKey"])
 		viper.Set(dbHostProp, vcapService.Credentials["host"])
 		viper.Set(dbUserProp, vcapService.Credentials["Username"])
 		viper.Set(dbPassProp, vcapService.Credentials["Password"])
 		viper.Set(dbNameProp, vcapService.Credentials["database_name"])
+
+		if contains(vcapService.Tags, "gcp") {
+			viper.Set(caCertProp, vcapService.Credentials["CaCert"])
+			viper.Set(clientCertProp, vcapService.Credentials["ClientCert"])
+			viper.Set(clientKeyProp, vcapService.Credentials["ClientKey"])
+		}
 	}
 }
 
@@ -58,10 +57,37 @@ func parseVcapServices(vcapServicesEnv string, logger lager.Logger) VcapService 
 			logger.Error("Error parsing VCAP_SERVICES environment variable", err)
 		}
 	}
-	if len(vcapServices) > 1 {
-		// TODO (hsophia): Change to logger.Error
-		logger.Info("The VCAP_SERVICES environment variable may only contain one database.")
+	index := findMySqlTag(vcapServices, "mysql")
+	if index == -1 {
+		logger.Info("The VCAP_SERVICES environment variable may only contain one MySQL database.")
 		os.Exit(1)
 	}
-	return vcapServices[0]
+	return vcapServices[index]
+}
+
+// contains tells whether a given string array arr contains string key
+func contains(arr []string, key string) bool {
+	for _, n := range arr {
+		if key == n {
+			return true
+		}
+	}
+	return false
+}
+
+// We'll want to search the list for credentials that have a tag of "mysql", fail if we find more or fewer than 1, and use the credentials there.
+func findMySqlTag(VcapServices []VcapService, key string) int {
+	index := -1
+	count := 0
+	for i, vcapService := range VcapServices {
+		if contains(vcapService.Tags, key) {
+			count += 1
+			index = i
+		}
+	}
+	if count != 1 {
+		return -1
+	} else {
+		return index
+	}
 }
