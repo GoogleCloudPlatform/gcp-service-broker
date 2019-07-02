e22831969d9d409cecc6fd409cdba34182b70923
Sophia Hu <hsophia@google.com>
2019-07-02 10:43:05 -0700

    Add comments in vcap.go

--- db_service/setup_db.go
+++ db_service/setup_db.go
@@ -69,6 +69,7 @@ func SetupDb(logger lager.Logger) *gorm.DB {
 	dbType := viper.GetString(dbTypeProp)
 	var db *gorm.DB
 	var err error
+	// if provided, use database injected by CF via VCAP_SERVICES environment variable
 	err = useVcapServices(logger)
 	if err != nil {
 		logger.Error("Invalid VCAP_SERVICES environment variable", err)
