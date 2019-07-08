e7f7af4cd954d7119376b50972732c5ab9a6962b
Sophia Hu <hsophia@google.com>
2019-07-08 16:25:35 -0700

    Refactor vcap.go to support integer arguments in credentials map

--- db_service/vcap.go
+++ db_service/vcap.go
@@ -1,7 +1,20 @@
+// Copyright 2019 the Service Broker Project Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package db_service
 
 import (
-	"code.cloudfoundry.org/lager"
 	"encoding/json"
 	"fmt"
 	"github.com/spf13/viper"
@@ -16,10 +29,10 @@ type VcapService struct {
 	Label        string            `json:"label"`         // The name of the service offering.
 	Tags         []string          `json:"tags"`          // An array of strings an app can use to identify a service instance.
 	Plan         string            `json:"plan"`          // The service plan selected when the service instance was created.
-	Credentials  map[string]string `json:"credentials"`   // The service-specific credentials needed to access the service instance.
+	Credentials  map[string]interface{} `json:"credentials"`   // The service-specific credentials needed to access the service instance.
 }
 
-func useVcapServices(logger lager.Logger) error {
+func UseVcapServices() error {
 	vcapData, vcapExists := os.LookupEnv("VCAP_SERVICES")
 
 	if !vcapExists {
@@ -30,19 +43,24 @@ func useVcapServices(logger lager.Logger) error {
 		return fmt.Errorf("Error parsing VCAP_SERVICES: %s", err)
 	}
 
-	u, err := url.Parse(vcapService.Credentials["uri"])
+	return setDatabaseCredentials(vcapService)
+}
+
+func setDatabaseCredentials(vcapService VcapService) error {
+
+	u, err := url.Parse(coalesce(vcapService.Credentials["uri"]))
 	if err != nil {
 		return fmt.Errorf("Error parsing credentials uri field: %s", err)
 	}
 
-	logger.Info("Using MySQL database injected via VCAP_SERVICES environment variable")
+	// Set up database credentials using environment variables
 	viper.Set(dbTypeProp, DbTypeMysql)
-	viper.Set(dbHostProp, vcapService.Credentials["host"])
+	viper.Set(dbHostProp, coalesce(vcapService.Credentials["host"], vcapService.Credentials["hostname"]))
 	viper.Set(dbUserProp, coalesce(u.User.Username(), vcapService.Credentials["Username"], vcapService.Credentials["username"]))
 	viper.Set(dbPassProp, coalesce(vcapService.Credentials["Password"], vcapService.Credentials["password"]))
 	viper.Set(dbNameProp, coalesce(vcapService.Credentials["database_name"], vcapService.Credentials["name"]))
 
-	//  if database is provided by gcp service broker, retrieve the client_cert, ca_cert and client_key fields
+	// If database is provided by gcp service broker, retrieve the client_cert, ca_cert and client_key fields
 	if contains(vcapService.Tags, "gcp") {
 		viper.Set(caCertProp, vcapService.Credentials["CaCert"])
 		viper.Set(clientCertProp, vcapService.Credentials["ClientCert"])
@@ -53,10 +71,15 @@ func useVcapServices(logger lager.Logger) error {
 }
 
 // Return first non-null string in list of arguments
-func coalesce(credentials ...string) string {
+func coalesce(credentials ...interface{}) string {
 	for _, credential := range credentials {
-		if credential != "" {
-			return credential
+		if credential != nil {
+			switch credential.(type) {
+			case int:
+				return fmt.Sprintf("%d", credential)
+			default:
+				return fmt.Sprintf("%v", credential)
+			}
 		}
 	}
 	return ""
